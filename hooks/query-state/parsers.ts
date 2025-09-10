d715b13823ca2c44d19b7e08146ac7b4c6ca96e4
pedro5g <pedluiz321@gmail.com>
2025-09-10 17:08:35 -0300

    feat: create parser to running in sever side
    - create parser with cache thats run in same page context
    - stric mode to throw Error if query params empty

--- hooks/query-state/parsers.ts
+++ hooks/query-state/parsers.ts
@@ -1,4 +1,9 @@
-import type { ParserConfig, UseQueryStateOptions } from "./types";
+import { cache } from "react";
+import type {
+  ParserConfig,
+  QueryStateConfig,
+  UseQueryStateOptions,
+} from "./types";
 
 // i based my implementation in:
 // @reference https://github.com/47ng/nuqs/blob/next/packages/nuqs/src/parsers.ts#L7
@@ -189,3 +194,213 @@ export const qsParserArray = <T>(
     },
   });
 };
+
+export type SearchParams = Record<string, string | string[] | undefined>;
+
+export type LoaderInput =
+  | URL
+  | Request
+  | URLSearchParams
+  | Record<string, string | string[] | undefined>
+  | string;
+
+type ParamsSchema<T> = {
+  [K in keyof T]: T[K];
+};
+
+const createLoader = <T extends Record<string, any>>(
+  parsers: QueryStateConfig<T>,
+  strict: boolean = false,
+) => {
+  // overloads
+  // ensures don't have type warns by promise don't resolved, we prevent this in fist if condition
+  function loadSearchParams(
+    input: LoaderInput,
+    strict: boolean,
+  ): ParamsSchema<T>;
+  function loadSearchParams(
+    input: Promise<LoaderInput>,
+    strict: boolean,
+  ): Promise<ParamsSchema<T>>;
+  function loadSearchParams(
+    input: LoaderInput | Promise<LoaderInput>,
+    strict: boolean,
+  ): Promise<ParamsSchema<T>> | ParamsSchema<T> {
+    // suspense/async support
+    // ensures loading.tsx or suspense its executed
+    if (input instanceof Promise) {
+      // resolver promise and reinvoke loadSearchParams
+      return input.then((i) => loadSearchParams(i, strict)) as Promise<
+        ParamsSchema<T>
+      >;
+    }
+
+    const searchParam = extractSearchParams(input);
+    const results = {} as {
+      [K in keyof T]: T[K];
+    };
+
+    for (const key in parsers) {
+      const parser = parsers[key] as UseQueryStateOptions<T[typeof key]>;
+
+      const query = searchParam.get(key);
+
+      if (query === null) {
+        if (strict && parser.defaultValue === undefined) {
+          throw new Error(
+            `Error while parsing query \`${query}\` for key \`${String(
+              key,
+            )}\`: [Tip]: Set a default value to prevent this  `,
+          );
+        }
+        results[key] = parser.defaultValue;
+        continue;
+      }
+
+      let parsedValue: T[typeof key] | null;
+      try {
+        parsedValue = parser.parse ? parser.parse(query) : (query as any);
+      } catch (error) {
+        if (strict) {
+          throw new Error(
+            `Error while parsing query \`${query}\` for key \`${String(
+              key,
+            )}\`: ${String(error)}`,
+          );
+        }
+        parsedValue = null;
+      }
+
+      if (strict && query && parsedValue === null) {
+        throw new Error(
+          `Failed to parse query \`${query}\` for key \`${String(
+            key,
+          )}\` (got null)`,
+        );
+      }
+
+      results[key] = parsedValue ?? parser.defaultValue;
+    }
+
+    return results;
+  }
+
+  return loadSearchParams;
+};
+
+function extractSearchParams(input: LoaderInput): URLSearchParams {
+  try {
+    if (input instanceof Request) {
+      return input.url
+        ? new URL(input.url).searchParams
+        : new URLSearchParams();
+    }
+    if (input instanceof URL) {
+      return input.searchParams;
+    }
+    if (input instanceof URLSearchParams) {
+      return input;
+    }
+    if (typeof input === "object") {
+      const searchParams = new URLSearchParams();
+      for (const [key, value] of Object.entries(input)) {
+        if (Array.isArray(value)) {
+          for (const v of value) {
+            searchParams.append(key, v);
+          }
+        } else if (value !== undefined) {
+          searchParams.set(key, value);
+        }
+      }
+      return searchParams;
+    }
+    if (typeof input === "string") {
+      if (URL.hasOwnProperty("canParse") && URL.canParse(input)) {
+        return new URL(input).searchParams;
+      }
+      return new URLSearchParams(input);
+    }
+  } catch {}
+  return new URLSearchParams();
+}
+
+// private property
+const $input: unique symbol = Symbol("INPUT");
+export function parseSearchParams<T extends Record<string, any>>(
+  parser: QueryStateConfig<T>,
+) {
+  const loader = createLoader(parser);
+
+  type Cache = {
+    searchParams: ParamsSchema<T>;
+    [$input]?: SearchParams;
+  };
+
+  const getCache = cache<() => Cache>(() => ({
+    searchParams: {} as ParamsSchema<T>,
+  }));
+
+  function parseSync(searchParams: SearchParams, strict: boolean = false) {
+    const cache = getCache();
+    if (Object.isFrozen(cache.searchParams)) {
+      if (cache[$input] && compareSearchParams(searchParams, cache[$input])) {
+        return all();
+      }
+      throw new Error(
+        "Search params cache already populated. Don't called `parse` twice",
+      );
+    }
+    cache.searchParams = loader(searchParams, strict);
+    cache[$input] = searchParams;
+    return Object.freeze(cache.searchParams);
+  }
+
+  function parse(
+    searchParam: SearchParams | Promise<SearchParams>,
+    strict: boolean = false,
+  ) {
+    if (searchParam instanceof Promise) {
+      return searchParam.then((searchParam) => parseSync(searchParam, strict));
+    }
+    return parseSync(searchParam, strict);
+  }
+
+  function all() {
+    const { searchParams } = getCache();
+    if (Object.keys(searchParams).length === 0) {
+      throw new Error(
+        "Empty search params cache. \n [Tips]: Search params can't be accessed in Layouts, it's one layer above. \n Try called `parser` in page.tsx before.",
+      );
+    }
+    return searchParams;
+  }
+
+  function get<K extends keyof T>(key: K): ParamsSchema<T>[K] {
+    const { searchParams } = getCache();
+    const entry = searchParams[key];
+    if (typeof entry === "undefined") {
+      throw new Error(
+        `Empty search params cache. Search params can't be accessed in Layouts. in get(${String(key)}) ` +
+          "\n [Tips]: Search params can't be accessed in Layouts, it's one layer above. \n Try called `parser` in page.tsx before.",
+      );
+    }
+    return entry;
+  }
+
+  return { parse, get, all };
+}
+
+export function compareSearchParams(a: SearchParams, b: SearchParams) {
+  if (Object.is(a, b)) {
+    return true;
+  }
+  if (Object.keys(a).length !== Object.keys(b).length) {
+    return false;
+  }
+  for (const key in a) {
+    if (a[key] !== b[key]) {
+      return false;
+    }
+  }
+  return true;
+}
